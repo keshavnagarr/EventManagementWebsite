5a20ddbf98d37008288bc0d3f79a2cad9d984273
keshavnagarr <152058273+keshavnagarr@users.noreply.github.com>
2024-07-26 21:30:48 +0530

    Add files via upload

---
+++ src/main/java/com/demo/model/User.java
@@ -0,0 +1,81 @@
+package com.demo.model;
+
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name="user1")
+public class User {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private String email;
+	@Column
+	private String message;
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", message=" + message + "]";
+	}
+}
